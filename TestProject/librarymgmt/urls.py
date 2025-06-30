421caf6ccb9938ffc2b550603e98894ba5212e9d
akriti-star <sharmaakriti8678@gmail.com>
2025-06-30 13:12:45 +0530

    commit

--- TestProject/librarymgmt/urls.py
+++ TestProject/librarymgmt/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
+
+from librarymgmt import views
+
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.BookListView.as_view(),name='listallbooks'),
+    path('book_detail/<int:pk>',views.BookDetailView.as_view(),name='bookdetail'),
+    path('add_book/',views.BookCreateView.as_view(),name='addnewbook'),
+    path('modify_book/<int:pk>',views.BookUpdateView.as_view(),name='modifybook'),
+    path('delete_book/<int:pk>',views.BookDeleteView.as_view(),name='deletebook'),
+
+
 ]
--- TestProject/librarymgmt/views.py
+++ TestProject/librarymgmt/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 # Create your views here.
+from librarymgmt.models import *
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'librarymgmt/listallbooks.html'
+    context_object_name = 'books'
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'librarymgmt/displaybook.html'
+    context_object_name = 'book'
+class BookCreateView(generic.CreateView):
+    model = Book
+    template_name = 'librarymgmt/addnewbook.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listallbooks')
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    template_name = 'librarymgmt/modifybookdetails.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listallbooks')
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'librarymgmt/deletebook.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listallbooks')
+
